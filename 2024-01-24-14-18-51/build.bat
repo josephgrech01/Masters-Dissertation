7f18da8de1d7aac83f13f32e8294493beef6f777
josephgrech01 <joseph.grech.20@um.edu.mt>
2024-01-26 12:46:12 +0100

    Experimented with OpenStreetMap data

---
+++ 2024-01-24-14-18-51/build.bat
@@ -0,0 +1,1 @@
+python "%SUMO_HOME%\tools\randomTrips.py" -n osm.net.xml --fringe-factor 5 -p 5.217141 -o osm.passenger.trips.xml -r osm.passenger.rou.xml -e 1000 --vehicle-class passenger --vclass passenger --prefix veh --min-distance 300 --trip-attributes "departLane=\"best\"" --fringe-start-attributes "departSpeed=\"max\"" --allow-fringe.min-length 1000 --lanes --validate
