4d72b762bb70bcad3be9057608077532c914b77f
josephgrech01 <joseph.grech.20@um.edu.mt>
2024-04-16 11:34:05 +0200

    Continued Training with FYP reward (100000 steps)

--- singapore/ppo.py
+++ singapore/ppo.py
@@ -7,8 +7,9 @@ print(device)
 e = sumoMultiLine(gui=False)
 
 # model = PPO('MlpPolicy', e, verbose=1, tensorboard_log='singapore/tensorboard/sidewalks/ppo1000000fypReward', device=device)
-model = PPO.load('ppo500000fypReward', tensorboard_log='singapore/tensorboard/sidewalks/ppo500000fypReward')
-model.learn(total_timesteps=500000, log_interval=1)
-model.save('singapore/models/sidewalks/ppo1000000fypReward')
+model = PPO.load('singapore/models/sidewalks/ppo500000fypReward', tensorboard_log='singapore/tensorboard/sidewalks/ppo500000fypReward/', device=device)
+model.set_env(e)
+model.learn(total_timesteps=500000, log_interval=1, reset_num_timesteps=False)
+model.save('singapore/models/sidewalks/ppo1500000fypReward')
 
 e.close()
