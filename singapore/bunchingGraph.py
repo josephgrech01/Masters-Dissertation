f85a276b0826785b3bae50a133e2b2a0a9b46321
josephgrech01 <joseph.grech.20@um.edu.mt>
2024-05-09 00:08:38 +0200

    More Bunching Graphs

--- singapore/bunchingGraph.py
+++ singapore/bunchingGraph.py
@@ -1,42 +1,95 @@
 import pickle
 import matplotlib.pyplot as plt
 
-with open('singapore/results/sidewalks/tls/normalFreq/nc/bunchingGraph2.pkl', 'rb') as f:
+with open('singapore/results/sidewalks/tls/normalFreq/ppo/bunchingGraph2.pkl', 'rb') as f:
     data = pickle.load(f)
 
-print(data)
+graph = '22'
 
-fig, ax = plt.subplots()
-labelled = {'Route 22': False, 'Route 43': False}
-for k, v in data.items():
-    x_values = []
-    y_values = []
-    for d in v:
-        # if (k[4:6] == '22' and d[1] > 8 and d[1] < 22) or (k[4:6] == '43' and d[1] > 22 and d[1] < 36): # original limits
-        if (k[4:6] == '22' and d[1] > 5 and d[1] < 27) or (k[4:6] == '43' and d[1] > 15 and d[1] < 45):
-            x_values.append(d[0]/3600 + 6.5)
-            if k[4:6] == '22':
-                y_values.append(d[1] - 8)
-                c = 'blue'
+if graph == 'shared':
+    fig, ax = plt.subplots()
+    fig.set_figwidth(10)
+    labelled = {'Route 22': False, 'Route 43': False}
+    for k, v in data.items():
+        x_values = []
+        y_values = []
+        for d in v:
+            # if (k[4:6] == '22' and d[1] > 8 and d[1] < 22) or (k[4:6] == '43' and d[1] > 22 and d[1] < 36): # original limits
+            if (k[4:6] == '22' and d[1] > 5 and d[1] < 27) or (k[4:6] == '43' and d[1] > 15 and d[1] < 45):
+                x_values.append(d[0]/3600 + 6.5)
+                if k[4:6] == '22':
+                    y_values.append(d[1] - 8)
+                    c = 'blue'
+                    label = 'Route 22'
+                else:
+                    y_values.append(d[1] - 22)
+                    c = 'red'
+                    label = 'Route 43'
+
+                # y_values.append(d[1])
+        if not labelled[label]:
+            plt.plot(x_values, y_values, color=c, linewidth=1, label=label)
+            labelled[label] = True
+        else:
+            plt.plot(x_values, y_values, color=c, linewidth=1)
+    ax.set_xlim(8,12)
+    ax.set_ylim(1,13)
+    plt.legend(loc=1)
+    plt.title('Shared Corridor Trajectories - No Control')
+    plt.xlabel('Time of Day')
+    plt.ylabel('Stop along Shared Corridor')
+    # plt.savefig('singapore/results/sidewalks/tls/normalFreq/shortNcSharedBunching.jpg')
+    plt.show()
+
+else:
+    fig, ax = plt.subplots()
+    fig.set_figwidth(10)
+    labelled = {'Route 22': False, 'Route 43': False}
+    for k, v in data.items():
+        x_values = []
+        y_values = []
+        for d in v:
+            # if (k[4:6] == '22' and d[1] > 8 and d[1] < 22) or (k[4:6] == '43' and d[1] > 22 and d[1] < 36): # original limits
+            if graph == '22':
                 label = 'Route 22'
-            else:
-                y_values.append(d[1] - 22)
-                c = 'red'
+                c = 'blue'
+                if k[4:6] == '22':
+                    x_values.append(d[0]/3600 + 6.5)
+                    y_values.append(d[1])
+                    # c = 'blue'
+                    # label = 'Route 22'
+                    
+                        
+            elif graph == '43':
                 label = 'Route 43'
+                c = 'red'
+                if k[4:6] == '43':
+                    x_values.append(d[0]/3600 + 6.5)
+                    y_values.append(d[1])
+                    # c = 'red'
+                    # label = 'Route 43'
 
-            # y_values.append(d[1])
-    if not labelled[label]:
-        plt.plot(x_values, y_values, color=c, linewidth=1, label=label)
-        labelled[label] = True
+        # print(graph)
+        # print(k[4:6])
+        # print(label)
+        if not labelled[label]:
+            plt.plot(x_values, y_values, color=c, linewidth=1, label=label)
+            labelled[label] = True
+        else:
+            plt.plot(x_values, y_values, color=c, linewidth=1)
+    ax.set_xlim(8,20)
+    
+    plt.legend(loc=1)
+    if graph == '22':
+        ax.set_ylim(0,33)
+        plt.title('Route 22 Trajectories - PPO with Weighted Reward')
     else:
-        plt.plot(x_values, y_values, color=c, linewidth=1)
-ax.set_xlim(8,12)
-ax.set_ylim(1,13)
-plt.legend(loc=1)
-plt.title('Shared Corridor Trajectories')
-plt.xlabel('Time of Day')
-plt.ylabel('Stop along Shared Corridor')
-plt.show()
+        ax.set_ylim(0,61)
+        plt.title('Route 43 Trajectories - PPO with Weighted Reward')
+    plt.xlabel('Time of Day')
+    plt.ylabel('Stop along Route')
+    plt.savefig('singapore/results/sidewalks/tls/normalFreq/ppoWeightedRewardRoute22Bunching.jpg')
+    plt.show()
 
 
     
