cfe2ffa2f9c176e4d977cfe44fbea26b13026b1f
josephgrech01 <joseph.grech.20@um.edu.mt>
2024-07-16 18:12:01 +0200

    No Control Bunching Graphs

--- scenario2/bunchingGraph.py
+++ scenario2/bunchingGraph.py
@@ -1,15 +1,17 @@
 import pickle
 import matplotlib.pyplot as plt
 
-with open('wang2020/results/continuous/timeReward/bunched/route1.pkl', 'rb') as f:
+with open('scenario2/results/noControl/noTraffic/route1.pkl', 'rb') as f:
     route1 = pickle.load(f)
-with open('wang2020/results/continuous/timeReward/bunched/route2.pkl', 'rb') as f:
+with open('scenario2/results/noControl/noTraffic/route2.pkl', 'rb') as f:
     route2 = pickle.load(f)
+with open('scenario2/results/noControl/noTraffic/route3.pkl', 'rb') as f:
+    route3 = pickle.load(f)
 
-strategy = 'PPO'
+strategy = 'No Control'
 bunched = False
 # save = None
-save = 'wang2020/results/graphs/continuous/timeReward/bunched/'
+save = 'scenario2/results/graphs/noControl/'
 
 for y in range(0, 6):
     for z in route1[y]:
@@ -63,6 +65,32 @@ else:
 plt.clf()
 
 
+for y in range(0, 6):
+    for z in route3[y]:
+        x_values = []
+        y_values = []
+
+        for i in z:
+            x_values.append((i[0]*9)/60)
+            y_values.append(i[1])
+
+        plt.plot(x_values, y_values, color='darkgreen')
+
+plt.yticks(range(1,13))
+if not bunched:
+    plt.title(strategy + ' - Route 3')
+else:
+    plt.title(strategy + ' - Route 3, Already Bunched')
+plt.xlabel('Time (mins)')
+plt.ylabel('Bus Stop')
+# plt.legend(loc=4)
+if save is not None:
+    plt.savefig(save + 'route3Bunching.jpg')
+else:
+    plt.show()
+plt.clf()
+
+
 labelled = [False, False]
 for y in range(0, 6):
     for z in route1[y]:
@@ -97,14 +125,64 @@ for y in range(0, 6):
 
 plt.yticks(range(1,4))
 if not bunched:
-    plt.title(strategy + ' - Shared Stops')
+    plt.title(strategy + ' - Shared Corridor 1')
 else:
-    plt.title(strategy + ' - Shared Stops, Already Bunched')
+    plt.title(strategy + ' - Shared Corridor 1, Already Bunched')
 plt.xlabel('Time (mins)')
 plt.ylabel('Bus Stop')
 plt.legend(loc=4)
 if save is not None:
-    plt.savefig(save + 'sharedStopsBunching.jpg')
+    plt.savefig(save + 'sharedStopsBunching1.jpg')
+else:
+    plt.show()
+plt.clf()
+
+
+
+
+
+labelled = [False, False]
+for y in range(0, 6):
+    for z in route2[y]:
+        x_values = []
+        y_values = []
+
+        for i in z:
+            if i[1] > 9:
+                x_values.append((i[0]*9)/60)
+                y_values.append(i[1]-9)
+
+        if not labelled[0]:
+            plt.plot(x_values, y_values, color='blue', label='Route 2', linewidth=1)
+            labelled[0] = True
+        else:
+            plt.plot(x_values, y_values, color='blue', linewidth=1)
+
+    for z in route3[y]:
+        x_values = []
+        y_values = []
+
+        for i in z:
+            if i[1] > 9:
+                x_values.append((i[0]*9)/60)
+                y_values.append(i[1]-9)
+
+        if not labelled[1]:
+            plt.plot(x_values, y_values, color='darkgreen', label='Route 3', linewidth=1)
+            labelled[1] = True
+        else:
+            plt.plot(x_values, y_values, color='darkgreen', linewidth=1)
+
+plt.yticks(range(1,4))
+if not bunched:
+    plt.title(strategy + ' - Shared Corridor 2')
+else:
+    plt.title(strategy + ' - Shared Corridor 2, Already Bunched')
+plt.xlabel('Time (mins)')
+plt.ylabel('Bus Stop')
+plt.legend(loc=4)
+if save is not None:
+    plt.savefig(save + 'sharedStopsBunching2.jpg')
 else:
     plt.show()
 plt.clf()
