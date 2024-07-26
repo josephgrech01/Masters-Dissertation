adaba1b43504f58206653a79f81b4edefdd70bd7
josephgrech01 <joseph.grech.20@um.edu.mt>
2024-07-26 16:20:29 +0200

    Final models for scenario2

--- scenario2/ppo.py
+++ scenario2/ppo.py
@@ -2,15 +2,15 @@ from stable_baselines3 import PPO
 
 from env import SumoEnv
 
-e = SumoEnv(gui=False, noWarnings=True, epLen=250, traffic=False, mixedConfigs=False, bunched=False, save=None, continuous=True)
+e = SumoEnv(gui=False, noWarnings=True, epLen=3000, traffic=False, mixedConfigs=False, bunched=False, save=None, continuous=True, headwayReward=False)
 
-# model = PPO("MlpPolicy", e, verbose=1, learning_rate=0.001, tensorboard_log="wang2020/tensorboard/continuous/timeReward/")
+# model = PPO("MlpPolicy", e, verbose=1, learning_rate=0.001, tensorboard_log="scenario2/tensorboard/continuous/timeReward")
 
-model = PPO.load('wang2020/models/continuous/timeReward150000', tensorboard_log="wang2020/tensorboard/continuous/timeReward/")
+model = PPO.load('scenario2/models/continuous/timeReward500000_2', tensorboard_log="scenario2/tensorboard/continuous/timeReward/2")
 model.set_env(e)
 
 
 model.learn(total_timesteps=50000, log_interval=1, reset_num_timesteps=False)
-model.save("wang2020/models/continuous/timeReward200000")
+model.save("scenario2/models/continuous/timeReward550000_2")
 
 e.close()
