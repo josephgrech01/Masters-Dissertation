84c3f331d993a7d3e0d251b509b6d3e5fe68b478
josephgrech01 <joseph.grech.20@um.edu.mt>
2024-07-28 01:26:34 +0200

    Running scenario 2 using trained models

--- scenario2/usingMaskablePPO.py
+++ scenario2/usingMaskablePPO.py
@@ -7,14 +7,14 @@ def mask_fn(env):
     return env.valid_action_mask()
 
 actions = ['Hold', 'Skip', 'Proceed']
-e = SumoEnv(gui=True, noWarnings=True, epLen=750, traffic=True, bunched=False, save='wang2020/results/maskablePPO/timeReward/traffic90/')
+e = SumoEnv(gui=True, noWarnings=True, epLen=3000, traffic=True, bunched=False, continuous=False, headwayReward=False, save=None)#'scenario2/results/discrete/timeReward/traffic90/')
 e = ActionMasker(e, mask_fn)
 # no traffic
 # model = PPO.load("models/ppoNoTraffic")
 
 # traffic
 # model=MaskablePPO.load("wang2020/models/maskablePPOupdatedHeadways200000dur15")
-model=MaskablePPO.load("wang2020/models/maskablePPOtimeReward275000")
+model=MaskablePPO.load("scenario2/models/maskablePPOtimeReward450000")
 # model=MaskablePPO.load("wang2020/models/maskablePPOmixedConfigs500000")
 
 obs = e.reset()
--- scenario2/usingPPO.py
+++ scenario2/usingPPO.py
@@ -2,13 +2,13 @@ from stable_baselines3 import PPO
 from env import SumoEnv
 
 
-e = SumoEnv(gui=True, noWarnings=True, epLen=750, traffic=True, bunched=False, continuous=True, save=None)#'wang2020/results/continuous/timeReward/traffic90/')
+e = SumoEnv(gui=True, noWarnings=True, epLen=3000, traffic=True, bunched=False, continuous=True, headwayReward=False, save=None)#'scenario2/results/continuous/timeReward/traffic90/')
 
 # model = PPO.load('singapore/models/sidewalks/ppo1800000fypReward')
 
 # model = PPO.load('singapore/models/sidewalks/weightedReward/normalFreq/ppo1750000WeightedRewardTLS')
 
-model = PPO.load('wang2020/models/continuous/timeReward200000')
+model = PPO.load('scenario2/models/continuous/timeReward550000_2')
 
 obs = e.reset()
 while True:
